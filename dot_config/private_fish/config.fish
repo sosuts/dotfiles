3df3d09f4ed7b1f9a16c1e6c4d78deaa43fd7dbb
sosuts <sosuke.utsunomiya@gmail.com>
2024-07-22 01:07:18 +0900

    update config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,33 +1,45 @@
-# Commands to run in interactive sessions can go here
 # if status is-interactive
 # end
 
 # Appearance
 set theme_color_scheme nord
 
+
+function fish_prompt
+    __vsc_fish_prompt
+    __vsc_fish_cmd_start
+    # add newline
+    echo -e "\n"
+end
+
 # Path
 fish_add_path ~/bin
-# export PATH="/home/sosuke/competitive-programming/.venv/bin:$PATH"
-# source "$HOME/.rye/env.fish"
 
-# pnpm
-set -gx PNPM_HOME "/home/sosuke/.local/share/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
-end
-# pnpm end
-
-# abbreviations
+# abbreviations for git
 abbr --add g git
-abbr --add ll "ls -al"
+# atcoder
 abbr --add ojs "oj submit main.py -l 5055 -y --no-open"
 abbr --add ojt "oj test -c 'python main.py'"
 abbr --add generate_random_input "oj generate-input 'python generate.py'"
 abbr --add generate_random_test "oj generate-output -c 'python naive.py'"
-# create a function that chain generate_random_input and generate_random_test.
-# the function will take one argument -n. -n is the number of test cases.
-# example: generate_random_case -n 10
+# docker
+abbr --add d docker
+abbr --add di docker image
+abbr --add dis docker images
+abbr --add dc docker container
+abbr --add dp docker compose
+abbr --add dl docker logs
+# clipboard
+abbr --add clip xsel --clipboard --input
+
 function generate_random_case
     generate_random_input $argv
     generate_random_test
+end
+
+function fish_prompt
+    __vsc_fish_prompt
+    __vsc_fish_cmd_start
+    # add newline
+    echo -e "\n"
 end
